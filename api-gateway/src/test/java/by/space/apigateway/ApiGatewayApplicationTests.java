df53a609ee268659b7435d2e2ba86e63f52ba612
Efrosiniya24 <efrosiniya24@gmail.com>
2025-11-10 22:05:35 +0300

    .

---
+++ api-gateway/src/test/java/by/space/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package by.space.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
