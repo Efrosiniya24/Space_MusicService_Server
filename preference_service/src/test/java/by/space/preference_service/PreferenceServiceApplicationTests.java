8f93de8ae3facc6b188c20c736f317dce4320d56
Efrosiniya24 <efrosiniya24@gmail.com>
2025-11-17 17:18:47 +0300

    preferences-service

---
+++ preference_service/src/test/java/by/space/preference_service/PreferenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.space.preference_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreferenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
